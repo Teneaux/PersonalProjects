0a4cf3bc506d36446c47c2f7ae064c079048bdbf
Santino Williams <saintofwilliam@gmail.com>
2025-01-23 14:32:05 -0500

    Made Cannonball Prefab
    Remade Cannon Mockup, made Cannonball Prefab, and commented in a brief breakdown of the launch process

--- Wreck'n'Roll/Assets/Scripts/LaunchBall.cs
+++ Wreck'n'Roll/Assets/Scripts/LaunchBall.cs
@@ -1,16 +1,11 @@
 using UnityEngine;
+//Script by: Santino Williams
+//Script functionalities should include:
+//On start oscillate the cannon barrel -> Move Cannon along Z-Axis, clamped at 25, and -20
+//Determine strength of the shot -> Determine a force variable from a set beginning and end range
+//Fire the shot ->  Instantiate the Cannonball object ->  Add Force to the Cannonball object based on the previously output force variable
 
 public class LaunchBall : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Ball cannonBall;
 }
