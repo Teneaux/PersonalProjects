bf3cfe961b63a6443ea04a8374dfa0ff6a3e8d87
Santino Williams <saintofwilliam@gmail.com>
2025-01-26 14:52:58 -0500

    Base Cannon Oscillation
    Functioning cannon oscillation, just need to accept input and shift to the next state. Variables are public for editing

--- Wreck'n'Roll/Assets/Scripts/CannonController.cs
+++ Wreck'n'Roll/Assets/Scripts/CannonController.cs
@@ -6,15 +6,68 @@ using UnityEngine;
 //Fire the shot ->  Instantiate the Cannonball object ->  Add Force to the Cannonball object based on the previously output force variable
 public class CannonController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform cannonRotation;
+    public float maxAngle = 30;
+    public float minAngle = -25;
+    public float oscillationSpeed;
+    public float defaultAngle;
+    public CannonStates activeState = CannonStates.Inactive;
+
+    GameManager manager;
+    public enum CannonStates
     {
-        
+        Inactive,
+        SettingAngle,
+        SettingPower,
+        Firing
     }
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        InactiveCannon();
+    }
+
     void Update()
     {
-        
+        switch (activeState)
+        {
+            case CannonStates.Inactive:
+                InactiveCannon();
+                break;
+            case CannonStates.SettingAngle:
+                OscillateCannon();
+                break;
+            case CannonStates.SettingPower:
+                CannonForce();
+                break;
+            case CannonStates.Firing:
+                FiringCannon();
+                break;
+        }
+    }
+
+    void InactiveCannon()
+    {
+        //Code for the inactive period goes here. Wait for timer to expire, and then game to start, move to next state.
+        activeState = CannonStates.SettingAngle;
+    }
+    void OscillateCannon()
+    {
+        float rotationRange = (maxAngle - minAngle) /2f;
+        float midpoint = minAngle + rotationRange;
+        defaultAngle = midpoint + Mathf.Sin(Time.time * oscillationSpeed) * rotationRange;
+        cannonRotation.localRotation = Quaternion.Euler(0,0,defaultAngle);
+    }
+    void CannonForce()
+    {
+
+    }
+    void FiringCannon()
+    {
+
     }
 }
+
+
+
