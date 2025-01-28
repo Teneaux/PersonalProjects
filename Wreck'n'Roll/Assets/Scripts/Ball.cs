b05629921efe2f4134971f580ca407768a33d834
Leger DeMain <josephwdubois@gmail.com>
2025-01-28 15:50:04 -0500

    Rename and Buidl of GameManager to PointManager Class
    Made the PointManager Class that tracks when a damageable object is destroyed (i.e. hp = 0). A DamageableObjects sends a OnObjectDeath event to the PointManager. The listener in the PointManager takes the MaxHP value from the argument e (The DamageableObjects object that died) and adds its MaxHP value to the currentScore variable in the Pointmanager and then Fires an event to the PointUIScript. The PointUIScript updates the UI to match the currentScore variable in the PointManager

--- Wreck'n'Roll/Assets/Scripts/Ball.cs
+++ Wreck'n'Roll/Assets/Scripts/Ball.cs
@@ -5,8 +5,7 @@ public class Ball : MonoBehaviour
     [SerializeField] private int Damage;
     [SerializeField] private float size;
     [SerializeField] private Rigidbody2D ballRigidBody;
-
-
+    
     public int GetDamageValue()
     {
         return Damage;
---
+++ Wreck'n'Roll/Assets/Scripts/UIScripts/PointUIScript.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using TMPro;
+
+public class PointUIScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private PointManager pointManager;
+    [SerializeField] private TMP_Text currentScoreText;
+    [SerializeField] private TMP_Text maximumScoreText;
+    void Start()
+    {
+        maximumScoreText.text = pointManager.GetMaximumPointAmount().ToString();
+        pointManager.OnCurrentScoreChange += PointManager_OnCurrentScoreChange;
+    }
+
+    private void PointManager_OnCurrentScoreChange(object sender, System.EventArgs e)
+    {
+        currentScoreText.text = pointManager.GetCurrentPointAmount().ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
